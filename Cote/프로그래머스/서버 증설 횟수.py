45da16e495945a1fdf7b06082850d523c5ccaf71
LeeJiYeon <80756039+leejiyeonnn@users.noreply.github.com>
2025-08-24 00:55:05 +0900

    Create 서버 증설 횟수.py

---
+++ Cote/프로그래머스/서버 증설 횟수.py
@@ -0,0 +1,13 @@
+def solution(players, m, k):
+    server = [0] * len(players)
+    cut = 0
+    
+    for i in range (len(players)):
+        remain = players[i]-(server[i]*m)
+        if remain >= m:
+            end = min(i+k, len(players))
+            for j in range(i, end):
+                server[j] += remain // m
+            cut += remain // m
+    
+    return cut
